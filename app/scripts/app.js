109964fa3fe1ad93782ee42c6421bc3e0c5d9a60
MarianaLimon <marianalimong@hotmail.com>
2016-03-22 17:55:14 -0600

    se agrego apartado de mensajes en la seccion de contacto y se modifico informacion de la pagina, se agrego archivo contactform.less

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,3 +8,13 @@
         'ui.bootstrap'                 // Bootstrap
     ])
 })();
+
+var app = angular.module('formApp', []);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.formData = {};
+
+    $scope.submitForm = function (formData) {
+        alert('Form submitted with' + JSON.stringify(formData));
+    };
+});
